05371e6852c6125a2987e836a26cb84798186ef1
KadenGordon <kaden.m.gordon@gmail.com>
2026-02-28 01:38:53 -0700

    update package

--- quartus/hdl/snake_package.vhd
+++ quartus/hdl/snake_package.vhd
@@ -4,10 +4,18 @@ use ieee.std_logic_1164.all;
 package snake_package is 
 
     type direction is (
-        UP, DOWN, LEFT, RIGHT
+        UP_DIR, DOWN_DIR, LEFT_DIR, RIGHT_DIR
     );
 
-    type row_t is array (0 to 4) of std_logic;
-    type position_board_t is array (0 to 3) of row_t;
+    type position_board_t is record
+        row : integer range 0 to NUM_ROWS-1;
+        col : integer range 0 to NUM_COLS-1;
+    end record;
+
+    type board_bool_t is array (0 to NUM_ROWS-1, 0 to NUM_COLS-1) of std_logic;
+
+    constant NUM_ROWS: integer := 14;
+    constant NUM_COLS: integer := 20;
+    constant NUM_CELLS: integer := NUM_ROWS * NUM_COLS;
 
 end package;
